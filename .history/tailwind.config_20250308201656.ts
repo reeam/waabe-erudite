bc8ebbabc69b1c5c743c4509a8ba3cd0dc5f1b73
reeam <ryo20030619@gmail.com>
2025-03-10 21:13:40 +0900

    Synced by Front Matter

---
+++ .history/tailwind.config_20250308201656.ts
@@ -0,0 +1,49 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+const config: Config = {
+  darkMode: ['selector'],
+  content: ['./src/**/*.{astro,md,mdx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'BIZ UDPGothic', ...defaultTheme.fontFamily.sans],
+        mono: ['Poppins', 'BIZ UDPGothic',...defaultTheme.fontFamily.mono],
+        notojp: ['Poppins', 'Noto Sans JP',...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        additive: {
+          DEFAULT: 'hsl(var(--additive))',
+          foreground: 'hsl(var(--additive-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        ring: 'hsl(var(--ring))',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
+}
+
+export default config
---
+++ src/content/authors/reeam.md
@@ -0,0 +1,9 @@
+---
+name: 'りーむ'
+avatar: 'https://1.gravatar.com/avatar/2d02746f2b333fd2387b2af5f03aeea267b56663ea2d54e75e59d6753b001653?size=256'
+bio: 'わああああ'
+website: 'https://reeam.one/'
+twitter: 'https://twitter.com/reeam_dora'
+github: 'https://github.com/reeam'
+mail: 'renraku@waabe.net'
+---
